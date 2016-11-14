f191c72d37fae612d21b0789beab5c3c51207fa6
victorhong1988 <victorhong1988@gmail.com>
2016-11-14 12:48:29 -0500

    create three image view

--- ScrollViewImageGalleries/ViewController.m
+++ ScrollViewImageGalleries/ViewController.m
@@ -10,6 +10,12 @@
 
 @interface ViewController ()
 
+@property (weak, nonatomic) IBOutlet UIScrollView *scrollView;
+
+@property UIImageView *imageOne;
+@property UIImageView *imageTwo;
+@property UIImageView *imageThree;
+
 @end
 
 @implementation ViewController
@@ -17,6 +23,23 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    self.imageOne = [[UIImageView alloc] initWithFrame: CGRectZero];
+    self.imageOne.translatesAutoresizingMaskIntoConstraints = NO;
+    self.imageOne.image = [UIImage imageNamed:@"Lighthouse-in-Field"];
+    [self.scrollView addSubview:self.imageOne];
+    
+    self.imageTwo = [[UIImageView alloc] initWithFrame: CGRectZero];
+    self.imageTwo.translatesAutoresizingMaskIntoConstraints = NO;
+    self.imageTwo.image = [UIImage imageNamed:@"Lighthouse-night"];
+    [self.scrollView addSubview:self.imageTwo];
+    
+    self.imageThree = [[UIImageView alloc] initWithFrame: CGRectZero];
+    self.imageThree.translatesAutoresizingMaskIntoConstraints = NO;
+    self.imageThree.image = [UIImage imageNamed:@"Lighthouse-zoomed"];
+    [self.scrollView addSubview:self.imageThree];
+    
+    [self.scrollView addConstraint:[NSLayoutConstraint constraintWithItem:self.imageOne attribute:<#(NSLayoutAttribute)#> relatedBy:<#(NSLayoutRelation)#> toItem:<#(nullable id)#> attribute:<#(NSLayoutAttribute)#> multiplier:<#(CGFloat)#> constant:<#(CGFloat)#>]]
 }
 
 
